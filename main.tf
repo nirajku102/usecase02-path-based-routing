90a9b460e31ca298775df456d55b51bf7e74e2bc
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-16 08:01:55 +0000

    adding health checks

--- main.tf
+++ main.tf
@@ -137,6 +137,17 @@ resource "aws_lb_target_group" "tg" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+  path                = "/"
+  port                = "80"
+  protocol            = "HTTP"
+  matcher             = "200"
+  interval            = 30
+  timeout             = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 5
+  }
 }
 
 # Attach EC2 Instances to Target Groups
