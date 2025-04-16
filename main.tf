565713354f9cca80ea491a0d8b25a32e364a30dd
nirajku102 <105897623+nirajku102@users.noreply.github.com>
2025-04-16 06:37:11 +0000

    adding alb and ec2

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "subnets" {
 
 resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.id
-    tage {
+    tags {
         name = "alb-igw"
     }
 }
@@ -68,13 +68,13 @@ resource "aws_security_group" "alb_sg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0./0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
     to_port = 0
-    protocol =  ["-1"]
+    protocol =  [-1]
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
